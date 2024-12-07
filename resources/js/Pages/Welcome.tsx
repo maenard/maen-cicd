c0b8a3e4551c7a6d202b64a4fa575fefb0e07140
Maenard Batulanon Abo-Abo <maenardaboabo23@gmail.com>
2024-12-07 23:07:56 +0800

    testing workflows

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -356,7 +356,7 @@ export default function Welcome({
                         </main>
 
                         <footer className="py-16 text-center text-sm text-black dark:text-white/70">
-                            Laravel v{laravelVersion} (PHP v{phpVersion})
+                            Laravel v{laravelVersion} (PHP v{phpVersion}) Hi
                         </footer>
                     </div>
                 </div>
